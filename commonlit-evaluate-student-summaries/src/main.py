4b134e6622c39c3acc4eb5df1ba2f5c1e84ec8b9
yuanfanwang <wang.far.sail@gmail.com>
2023-09-04 15:54:00 +0900

    split

--- commonlit-evaluate-student-summaries/src/main.py
+++ commonlit-evaluate-student-summaries/src/main.py
@@ -1,7 +1,9 @@
+import re
+import torch
+
 import numpy as np
 import pandas as pd
-import torch
-import re
+
 from spellchecker import SpellChecker
 
 prompts_test    = pd.read_csv('data/prompts_test.csv')
@@ -10,14 +12,22 @@ summaries_train = pd.read_csv('data/summaries_train.csv')
 summaries_test  = pd.read_csv('data/summaries_test.csv')
 
 allowed_symbol = r'[^a-zA-Z0-9\s\"\'\-\;\.\,]'
+
 for Text in summaries_train['text']:
     text = Text.lower()
     text = text.rstrip()
+
     text = text.replace('"', ' " ')
     text = text.replace(';', ' ; ')
     text = text.replace('.', ' . ')
     text = text.replace(',', ' , ')
     text = re.sub(allowed_symbol, ' ', text)
     text = re.sub(r'\s{2,}', ' ', text)
+
     if not text.endswith(' . '):
         text += '. '
+    
+    # split text
+    splited_text = text.split(' ')
+    print(splited_text)
+    break
