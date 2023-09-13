f79c4e705a2d54b99a208c896b82570ad6092f75
yuanfanwang <wang.far.sail@gmail.com>
2023-09-14 01:28:44 +0900

    devertav3 + LGBM

--- commonlit-evaluate-student-summaries/src/main.py
+++ commonlit-evaluate-student-summaries/src/main.py
@@ -1,3 +1,5 @@
+# https://www.kaggle.com/code/farsail/devertav3-lgbm/edit
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import warnings
