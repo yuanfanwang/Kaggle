46806265ed91153a6e73d11c14f1424bf2ab3f0c
yuanfanwang <wang.far.sail@gmail.com>
2024-03-11 00:44:25 +0900

    rename

--- pii-detection-removal-from-educational-data/src/baseline.py
+++ pii-detection-removal-from-educational-data/src/baseline.py
@@ -1,304 +1,252 @@
-import json
+"""
+!pip install "/kaggle/input/seqeval/seqeval-1.2.2-py3-none-any.whl"
+"""
+
 import sys
-import os
 import torch
-import torch.nn as nn
+
 import numpy as np
+import pandas as pd
 import polars as pl
-from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding, AutoModelForSequenceClassification
-from datasets import Dataset
-from torch.utils.data import Dataset as tds
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold
-from transformers.modeling_outputs import ModelOutput
 
+from datasets import Dataset, DatasetDict, load_metric
+from huggingface_hub import notebook_login
+from sklearn.model_selection import GroupKFold
+from tqdm import tqdm
+from transformers import AutoTokenizer, DataCollatorForTokenClassification, AutoModelForTokenClassification, TrainingArguments, Trainer
+from seqeval.metrics import precision_score, recall_score, accuracy_score, f1_score
+
+
+""" memo
+# input_ids 1, 2 が trainer.evaluate() でどのように評価されるか
+# whitespace, " ", "\n\n" などの対応
+# seed 固定
+# seperate a long token
+"""
+
+torch.cuda.empty_cache()
 np.object = object
-
-class Env:
-    local = True
-
-cache_dataset = True
-
-if Env.local:
-    train_json_file_path = "./data/train.json"
-    test_json_file_path  = "./data/test.json"
-else:
-    train_json_file_path = "kaggle/train.json"
-    test_json_file_path = "kaggle/test.json"
-
-
-pii_to_num = {
-    "O": 0,
-    "B-NAME_STUDENT": 1,
-    "I-NAME_STUDENT": 2,
-    "B-EMAIL": 3,
-    "I-EMAIL": 4,
-    "B-USERNAME": 5,
-    "I-USERNAME": 6,
-    "B-ID_NUM": 7,
-    "I-ID_NUM": 8,
-    "B-PHONE_NUM": 9,
-    "I-PHONE_NUM": 10,
-    "B-URL_PERSONAL": 11,
-    "I-URL_PERSONAL": 12,
-    "B-STREET_ADDRESS": 13,
-    "I-STREET_ADDRESS": 14,
-}
-
-num_to_pii = {
-    1 : "O",
-    2 : "B-NAME_STUDENT",
-    3 : "I-NAME_STUDENT",
-    4 : "B-EMAIL",
-    5 : "I-EMAIL",
-    6 : "B-USERNAME",
-    7 : "I-USERNAME",
-    8 : "B-ID_NUM",
-    9 : "I-ID_NUM",
-    10: "B-PHONE_NUM",
-    11: "I-PHONE_NUM",
-    12: "B-URL_PERSONAL",
-    13: "I-URL_PERSONAL",
-    14: "B-STREET_ADDRESS",
-    15: "I-STREET_ADDRESS",
-}
-
-config = {
-    'model': 'microsoft/deberta-v3-base'
-}
-
-
-####################################
-# Tokenizer
-####################################
-class CustomTokenizer:
-    def __init__(self, max_length=20):
-        self.tokenizer = AutoTokenizer.from_pretrained(config['model'])
-        self.max_length = max_length
-    
-    def remove_special_tokens(self, tokens):
-        tokens.pop(0)
-        tokens.pop(-1)
-        return tokens
-
-    def add_special_tokens(self, tokens):
-        tokens.insert(0, 1)
-        tokens.append(2)
-        return tokens 
-
-    # TODO: corresponding to max length
-    def __call__(self, data):
-        model_ids = []
-        model_labels = []
-        label_conversion_key = []
-
-        for i, whitespace in enumerate(data['trailing_whitespace']):
-            if whitespace:
-                data['tokens'][i+1] += ' '
-
-        for token, label in zip(data.get('tokens', []), data.get('labels', [])):
-            input = self.tokenizer(token, padding="max_length", truncation=True)
-            input_ids = input['input_ids']
-            input_ids = self.remove_special_tokens(input_ids)
-
-            model_ids += input_ids
-            if 'labels' in data:
-                model_labels += [pii_to_num[label]] * len(input_ids)
-            label_conversion_key.append(len(input_ids))
-
-            # trancate
-            if len(model_ids) > self.max_length:
-                max_sz = self.max_length-2
-                model_ids = model_ids[:max_sz]
-                model_labels = model_labels[:max_sz]
-                label_conversion_key = label_conversion_key[:max_sz]
-                break
-
-        # TODO: model_label doens't have lables for special tokens at the beginnig and the end
-        model_ids = self.add_special_tokens(model_ids)
-        data["labels"] = model_labels
-        # TODO: attention_mask should be 1 for only the size of the tokens
-        additional = {
-            'input_ids' : model_ids,
-            'token_type_ids': [0] * len(model_ids),
-            'attention_mask': [1] * len(model_ids),
-            'label_conversion_key': label_conversion_key
-        }
-        return {**data, **additional}
-
-
-def make_submission_labels(label_conversion_key, output_labels):
-    submission_labels = []
-    prev_idx = 0
-    for l in label_conversion_key:
-        token_label = output_labels[prev_idx: prev_idx + l]
-        # use first label for now
-        submission_labels.append(token_label[0])
-        prev_idx += l
-
-    return submission_labels
-
-
-####################################
-# Datasets 
-####################################
-tokenizer = CustomTokenizer()
-if cache_dataset:
-    train_dataset = Dataset.load_from_disk("/home/competitions/pii-detection-removal-from-educational-data/data/dataset/tokenized_train_20")
-    test_dataset = Dataset.load_from_disk("/home/competitions/pii-detection-removal-from-educational-data/data/dataset/tokenized_test_20")
-    # train_dataset = train_dataset.remove_columns(["labels"])
-    # test_dataset = test_dataset.remove_columns(["labels"])
-    # print(train_dataset[:5])
-    # print(test_dataset[:5])
-else:
-    train_data = pl.read_json(train_json_file_path).to_pandas()   # document,
-                                                                  # full_text,
-                                                                  # tokens,
-                                                                  # trailing_whitespace,
-                                                                  # labels
-    train_dataset = Dataset.from_pandas(train_data)
-    train_dataset = train_dataset.map(tokenizer.__call__)
-    train_dataset = train_dataset.remove_columns(["document", "full_text", "tokens", "trailing_whitespace", "label_conversion_key"])
-    train_dataset.save_to_disk("/home/competitions/pii-detection-removal-from-educational-data/data/dataset/tokenized_train_20")
-
-    test_data  = pl.read_json(test_json_file_path).to_pandas()
-    test_dataset = Dataset.from_pandas(test_data)
-    test_dataset = test_dataset.map(tokenizer.__call__)
-    test_dataset = test_dataset.remove_columns(["document", "full_text", "tokens", "trailing_whitespace", "label_conversion_key"])
-    test_dataset.save_to_disk("/home/competitions/pii-detection-removal-from-educational-data/data/dataset/tokenized_test_20")
-
-train_dataset = train_dataset.with_format("torch")
-test_dataset = test_dataset.with_format("torch")
-
-####################################
-# Model
-####################################
-class FbetaLoss(nn.Module):
-    def __init__(self, beta=5):
-        super().__init__()
-        self.beta = beta
-    
-    # micro F beta
-    def forward(self, targets, labels):
-        if targets.shape != labels.shape:
-            raise ValueError("targets and labels should have the same shape")
-
-        true_positive = 1e-9
-        false_positive = 1e-9
-        false_negative = 1e-9
-        true_negative = 1e-9
-        for label, target in zip(labels[0], targets[0]):
-            if label != 0 and target != 0:
-                true_positive += 1
-            elif label == 0 and target != 0:
-                false_positive += 1
-            elif label != 0 and target == 0:
-                false_negative += 1
-            elif label == 0 and target == 0:
-                true_negative += 1
-
-        precision = true_positive / (true_positive + false_positive)
-        recall = true_positive / (true_positive + false_negative)
-        Fbeta = (1 + self.beta**2) * (precision * recall) / (self.beta**2 * precision + recall)
-        return Fbeta
-
-class CustomModel(nn.Module):
-    def __init__(self, pretrained_model, loss_function, label_size):
-        super().__init__()
-        self.debartav3base = pretrained_model
-        self.hidden_size = self.debartav3base.config.hidden_size
-        self.liner = nn.Linear(self.hidden_size, label_size)
-        self.loss_function = loss_function
-
-    # witout labels
-    def forward(self, input_data):
-        print('labels' in input_data)
-        labels = input_data.pop("labels")
-        print('labels' in input_data)
-        outputs = self.debartav3base(**input_data)
-        targets = outputs.last_hidden_state[:, 0, :]  # shape: [1, 20, 768] -> [1, 768]
-        targets = self.liner(targets)
-        targets = torch.round(targets)
-
-        loss=None
-        if labels is not None:
-            loss = self.loss_function(targets, labels)
-        else:
-            print("Error: labels not in input_data or loss_function is None")
-        
-        return ModelOutput(
-            logits=targets,
-            loss=loss,
-            last_hidden_state=outputs.last_hidden_state,
-            attentions=None,
-            hidden_states=None
-        )
-
-"""
-class CustomModel(nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-        self.liner = nn.Linear(768, 1)
- 
-    def forward(self, inputs):
-        labels = inputs.pop("labels")
-        outputs = self.model(**inputs)
-        targets = outputs.last_hidden_state[:, 0, :]
-        targets = self.liner(targets)
-        loss = nn.L1Loss(targets, labels)
-
-        return ModelOutput(
-            loss=loss,
-            logits=targets,
-            last_hidden_state=outputs.last_hidden_state,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
-"""
-
-
-class CustomDataCollator(DataCollatorWithPadding):
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(config['model'])
-
-
-model = AutoModelForSequenceClassification.from_pretrained(config['model'])
-fbeta5_loss = FbetaLoss()
-custom_model = CustomModel(model, fbeta5_loss, 18)
-custom_data_collator = CustomDataCollator()
-print(custom_model(train_dataset[:1]))
-sys.exit()
-training_args = TrainingArguments(
-    output_dir="trained_model",
-    evaluation_strategy="epoch",
-    report_to="none",  # for wandb
-)
-
-trainer = Trainer(
-    model=custom_model,
-    args=training_args,
-    train_dataset=train_dataset[:50],
-    eval_dataset=test_dataset[:10],
-    data_collator=custom_data_collator
-)
-
-####################################
-# Train 
-####################################
-trainer.train()
-# trainer.predict(test_dataset[:10])
-
-"""
-####################################
-# Evaluation 
-####################################
-eval_results = trainer.evaluate()
-print(eval_results)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = model.to(device)
-input_text = "Text"
-input_data = tokenizer(input_text, return_tensors='pt').to(device)
-outputs = model(**input_data)
-outputs.logits.argmax(-1).item()
-"""
+
+Local = False
+if Local:
+    data_path = "data/"
+else:
+    data_path = "/kaggle/input/pii-detection-removal-from-educational-data/"
+
+model_checkpoint = "/kaggle/input/debertav3base"
+label_names = [
+    "O",
+    "B-NAME_STUDENT",
+    "I-NAME_STUDENT",
+    "B-EMAIL",
+    "I-EMAIL",
+    "B-USERNAME",
+    "I-USERNAME",
+    "B-ID_NUM",
+    "I-ID_NUM",
+    "B-PHONE_NUM",
+    "I-PHONE_NUM",
+    "B-URL_PERSONAL",
+    "I-URL_PERSONAL",
+    "B-STREET_ADDRESS",
+    "I-STREET_ADDRESS",
+]
+id2label = {str(i): label for i, label in enumerate(label_names)}
+label2id = {label: i for i, label in enumerate(label_names)}
+
+class CFG:
+    sample_data_size = 1000
+    batch_size = 1
+    token_max_length = 1024
+    epoch = 3
+    fold = 4
+
+def align_labels_with_tokens(labels, word_ids):
+    new_labels = []
+    current_word = None
+    for word_id in word_ids:
+        if word_id != current_word:
+            current_word = word_id
+            label = -100 if word_id is None else labels[word_id]
+            new_labels.append(label)
+        elif word_id is None:
+            new_labels.append(-100)
+        else:
+            label = labels[word_id]
+            if label % 2 == 1:
+                label += 1
+            new_labels.append(label)
+    return new_labels
+
+def tokenize_and_align_labels(examples):
+    tokenized_inputs = tokenizer(
+        examples["tokens"], truncation=True, is_split_into_words=True, max_length=CFG.token_max_length
+    )
+    word_ids = [tokenized_inputs.word_ids(i) for i in range(len(tokenized_inputs["input_ids"]))]
+    tokenized_inputs["word_ids"] = word_ids
+
+    # return when it is test dataset
+    if "labels" not in examples: return tokenized_inputs
+
+    # modify labels for train data
+    all_labels = examples["labels"]
+    new_labels = []
+    for i, labels in enumerate(all_labels):
+        word_ids = tokenized_inputs.word_ids(i)
+        labels = [label2id[label] for label in labels]
+        new_labels.append(align_labels_with_tokens(labels, word_ids))
+    tokenized_inputs["labels"] = new_labels
+    return tokenized_inputs
+
+def add_fold_column(example):
+    # return when it is test dataset
+    if 'labels' not in example: return example
+    # just want to know the length of the example to add fold column
+    eg_len = len(example['input_ids'])
+    folds = np.random.randint(0, CFG.fold, eg_len)
+    example['fold'] = folds
+    return example
+
+
+def make_dataset():
+    train_df = pl.read_json(data_path + "train.json").to_pandas()
+    # train_df = train_df[:CFG.sample_data_size]
+    train_dataset = Dataset.from_pandas(train_df)
+    test_df = pl.read_json(data_path + "test.json").to_pandas()
+    test_dataset = Dataset.from_pandas(test_df)
+    datasets = DatasetDict({
+        "train": train_dataset,
+        "test": test_dataset
+    })
+
+    datasets = datasets.map(
+        tokenize_and_align_labels,
+        batched=True,
+    )
+
+    datasets = datasets.map(
+        add_fold_column,
+        batched=True,
+    )
+
+    return datasets
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+tokenized_datasets = make_dataset()
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+def f_score(precision, recall, beta=1):
+    epsilon = 1e-7
+    return (1 + beta ** 2) * (precision * recall) / (beta ** 2 * precision + recall + epsilon)
+
+def compute_metrics(eval_preds):
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+
+    true_labels = [[label_names[l] for l in label if l != -100] for label in labels]
+    true_predictions = [
+        [label_names[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    return {
+        "precision": precision_score(true_labels, true_predictions),
+        "recall": recall_score(true_labels, true_predictions),
+        "f1": f1_score(true_labels, true_predictions),
+        "f5": f_score(precision_score(true_labels, true_predictions), recall_score(true_labels, true_predictions), 5),
+        "accuracy": accuracy_score(true_labels, true_predictions),
+    }
+
+
+best_f5_score = -1.0
+best_trainer = None
+gkf = GroupKFold(n_splits=CFG.fold)
+gkf_dataset = gkf.split(X=tokenized_datasets['train'],
+                        y=tokenized_datasets['train']['labels'],
+                        groups=tokenized_datasets['train']['fold'])
+for i, (train_index, valid_index) in enumerate(gkf_dataset):
+    print(f"\nFold {i}")
+    train_dataset = tokenized_datasets['train'].select(train_index)    
+    valid_dataset = tokenized_datasets['train'].select(valid_index)
+
+    model = AutoModelForTokenClassification.from_pretrained(
+        model_checkpoint, id2label=id2label, label2id=label2id).to(device)
+
+    args = TrainingArguments(
+        disable_tqdm=False,
+        output_dir=f"bert-finetune-ner_{i}",
+        # evaluation_strategy="epoch",
+        evaluation_strategy="steps",
+        eval_steps=1000,
+        # fp16=True,
+        save_strategy="no",
+        per_device_train_batch_size=CFG.batch_size,  # 1 is not out of memory
+        per_device_eval_batch_size=CFG.batch_size,   # 1 is not out of memory
+        learning_rate=2e-5,
+        num_train_epochs=CFG.epoch,
+        weight_decay=0.01,
+        push_to_hub=False,
+        report_to="none",
+        log_level="error",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=valid_dataset,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+    )
+
+    # train model
+    trainer.train()
+
+    # evaluate model with valid dataset to get the best model
+    eval_result = trainer.evaluate()
+    print(f"f5: {eval_result['eval_f5']}")
+
+    # TODO: for ansamble (but this process takes about 20min if the size of the test dataset is 20,000.)
+    # prediction = trainer.predict(tokenized_datasets['test'])
+    # label_predictions = np.argmax(prediction.predictions, axis=-1)
+
+    if best_f5_score < eval_result['eval_f5']:
+        best_f5_score = eval_result['eval_f5']
+        best_trainer = trainer
+
+
+# predict labels 
+test_datasets     = tokenized_datasets['test']
+prediction        = best_trainer.predict(test_datasets)  # PredictionOutput Object
+label_predictions = np.argmax(prediction.predictions, axis=-1)  # (data_size, token max length)
+
+# create submission file
+row_id_sub, document_sub, token_sub, label_sub = [], [], [], []
+test_df = pd.DataFrame(test_datasets)
+for i, labels in enumerate(label_predictions):
+    word_ids = test_df.at[i, 'word_ids']
+    document = test_df.at[i, 'document']
+    current_id = None
+    for j, (pii_idx, word_id) in enumerate(zip(labels, word_ids)):
+        if word_id == None: continue
+        if current_id != word_id:
+            current_id = word_id
+            if pii_idx != 0:
+                row_id_sub.append(len(row_id_sub))
+                document_sub.append(document)
+                token_sub.append(word_id)
+                label_sub.append(label_names[pii_idx])
+
+submission = {
+    'row_id': row_id_sub,
+    'document': document_sub,
+    'token': token_sub,
+    'label': label_sub
+}
+
+submission = pd.DataFrame(submission)
+print(submission.head())
+submission.to_csv("submission.csv", index=False)
