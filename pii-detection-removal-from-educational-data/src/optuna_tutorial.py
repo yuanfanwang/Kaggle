7b469369af8dba817aa0692065b2aefbb3365402
yuanfanwang <wang.far.sail@gmail.com>
2024-03-17 19:55:55 +0900

    optuna tutorial

--- pii-detection-removal-from-educational-data/src/optuna_tutorial.py
+++ pii-detection-removal-from-educational-data/src/optuna_tutorial.py
@@ -1,13 +1,23 @@
 import optuna
 
-def objective(trial):
-    x = trial.suggest_uniform('x', -10, 10)
+def calc(hyparam):
+    x = hyparam['x']
     return (x - 2) ** 2
 
-study = optuna.create_study()
-study.optimize(objective, n_trials=100)
+def objective(trial):
+    hyparam = {
+        'x': trial.suggest_uniform('x', -10, 10)
+    }
+    res = calc(hyparam) 
+    return res
 
-best_params = study.best_params
-best_value = study.best_value
-print("best_params: ", best_params)
-print("best_value: ", best_value)
+def run_optuna():
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=100)
+
+    best_params = study.best_params
+    best_value = study.best_value
+    print("best_params: ", best_params)
+    print("best_value: ", best_value)
+
+run_optuna()
